c0fd8f4c201227e20caacb6f0cf8164eb251768f
abhisheksingh01-ai <abhishek.ai.learn@gmail.com>
2026-03-23 10:04:32 +0530

    origin

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const enquiryRoutes = require('./routes/enquiry.routes');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://client-project-01-six.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
